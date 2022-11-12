b91d4c74e12ed0a4b0c9d29a8f5b111b31367006
Bomsitex <bomaepele@yahoo.com>
2022-11-12 15:28:44 +0000

    tasks 0, 1 & 2 updated and betty-checked, running ok)

--- main.h
+++ main.h
@@ -11,13 +11,19 @@
 #include <errno.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/wait.h>
+
+extern char **environ;
 
 /* team library functions */
 void buffchar(int ch, char *buff);
 int _putchar(char c);
-int _strlen(char *s);
+size_t _strlen(char *s);
 char *_strncat(char *dest, char *src, int n);
 char *_strchr(char *s, char c);
+char **splitLine(char **cmdLine, char ***args);
+ssize_t _getline(char **lineptr, size_t *n, int fd);
+char **parseLine(char *str, char *delim);
 
 
 #endif /* _SIMPLE_SHELL_BK_ */
